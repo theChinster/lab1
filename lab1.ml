0104cd4f6cb9ed43be90183e9842a63ceeee44d6
Alexander Chin <alexanderchin@college.harvard.edu>
2018-02-01 16:40:32 -0500

    fixed unused variable compiler warnings

--- lab1.ml
+++ lab1.ml
@@ -204,7 +204,7 @@ that, zip [1] [2; 3; 4] = [(1, 2); (false, 3); (false, 4)]?
 let rec length (lst : int list) : int =
   match lst with
   | [] -> 0
-  | head :: tail -> 1 + length tail ;;
+  | _head :: tail -> 1 + length tail ;;
 
 let rec zip (x : int list) (y : int list) : (int * int) list =
   match x, y with
